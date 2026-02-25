39f291d6da6596276eb6740b9f9fde6bed699d6f
Riccardo Lunardelli <rickylunardelli@gmail.com>
2026-02-25 14:27:13 +0100

    ADD: api per get lista e preview di dizionario, template base, kb, template

--- backend_api/server_api.py
+++ backend_api/server_api.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from uuid import uuid4
 from datetime import datetime, timezone, timedelta
+from pathlib import Path
+import json
 
 from src.intermediateLayer.postgres_repository import RunRepository, UsersRepository, BatchesRepository
 
@@ -14,10 +16,33 @@ class LoginRequest(BaseModel):
 
 app = FastAPI()
 
+TEMPLATE_DIR = Path("/home/ricky-lu/rickylu-workspace/ProgettiAI/Progetto-MCP/pv_datas/templates")
+DICTIONARIES_DIR = Path("/home/ricky-lu/rickylu-workspace/ProgettiAI/Progetto-MCP/data/dictionaries")
+KB_DIR = Path("/home/ricky-lu/rickylu-workspace/ProgettiAI/Progetto-MCP/data/kb")
+TEMPLATE_BASE_DIR = Path("/home/ricky-lu/rickylu-workspace/ProgettiAI/Progetto-MCP/data/template_base")
+
 dsn = "dbname=semantic_ai_mapper user=semantic_user password=semantic_password host=localhost port=5432"
 runClass = RunRepository(dsn)
 userClass = UsersRepository(dsn)
 batchClass = BatchesRepository(dsn)
+
+def list_artifact(artifact, artifact_dir):
+    # restituisce lista dei file presenti in una cartella
+
+    if not artifact_dir.exists():
+        raise HTTPException(status_code=404, detail=f"{artifact} directory not found !")
+    files = sorted([p.name for p in artifact_dir.glob("*.json")])
+    return {f"{artifact}": files}
+
+def get_file_of_artifact(name: str, artifact, artifact_dir):
+    # ritorna contenuto file
+
+    path = artifact_dir / name
+    if not path.exists() or not path.is_file():
+        raise HTTPException(status_code=404, detail=f"{artifact} not found!")
+    with open(path, "r", encoding="utf-8") as f:
+        content = json.load(f)
+    return {"name": name, "content": content}
 
 #-----ENDOPOINT-------
 @app.get("/")
@@ -59,3 +84,42 @@ def get_run(run_id: str):
     except KeyError:
         raise HTTPException(status_code=404, detail="run not found!")
     return query
+
+# get templates
+@app.get("/templates")
+def list_templates():
+    return list_artifact("template", TEMPLATE_DIR)
+
+# preview template
+@app.get("/templates/{name}")
+def get_template(name: str):
+    return get_file_of_artifact(name, "template", TEMPLATE_DIR)
+
+# get dizionari
+@app.get("/dictionaries")
+def list_dictionaries():
+    return list_artifact("dictionary", DICTIONARIES_DIR)
+
+# preview dizionario
+@app.get("/dictionaries/{name}")
+def get_dictionary(name: str):
+    return get_file_of_artifact(name, "dictionary", DICTIONARIES_DIR)
+
+# get kb
+@app.get("/kb")
+def list_kb():
+    return list_artifact("kb", KB_DIR)
+
+# preview kb
+@app.get("/kb/{name}")
+def get_kb(name: str):
+    return get_file_of_artifact(name, "kb", KB_DIR)
+
+# get template base
+@app.get("/template_base")
+def list_template_base():
+    return list_artifact("template_base", TEMPLATE_BASE_DIR)
+
+@app.get("/template_base/{name}")
+def get_template_base(name: str):
+    return get_file_of_artifact(name, "template_base", TEMPLATE_BASE_DIR)
