4e0f077aeb64027e2b4701216264e7250f89372f
Riccardo Lunardelli <rickylunardelli@gmail.com>
2026-03-06 16:50:08 +0100

    feat(fe): Aggiunta card history

--- frontend/src/components/Home/HomePage.tsx
+++ frontend/src/components/Home/HomePage.tsx
@@ -495,19 +495,90 @@ function HomePageTag() {
             </span>
           </div>
         </div>
-        {/* Test */}
+        {/* Cronologia */}
         <div
           style={{
-            backgroundColor: "#ffffff",
-            border: "1px solid #e5e7eb",
             borderRadius: "10px",
             height: "180px",
             padding: "15px",
-            cursor: "not-allowed",
+            display: "flex",
+            flexDirection: "column",
           }}
           className="HoverTransform"
         >
-          Card RUN todo
+          {/* Icona + testo */}
+          <div
+            style={{
+              marginLeft: "10px",
+              marginTop: "8px",
+              height: "80px",
+              width: "60px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {/* Icona */}
+            <div
+              style={{
+                height: "60px",
+                width: "60px",
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <span
+                className="material-symbols-outlined"
+                style={{ fontSize: "40px", color: "#3268c5", zIndex: 1 }}
+              >
+                history
+              </span>
+
+              <div
+                style={{
+                  position: "absolute",
+                  height: "100%",
+                  width: "100%",
+                  backgroundColor: "#477dda",
+                  opacity: "0.2",
+                  borderRadius: "6px",
+                }}
+              />
+            </div>
+          </div>
+          {/* Testo sotto icona */}
+          <div
+            style={{
+              marginLeft: "10px",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <span
+              style={{
+                fontSize: "20px",
+                fontWeight: 500,
+                color: "var(--black)",
+              }}
+            >
+              History
+            </span>
+            <span
+              style={{
+                fontSize: "14px",
+                fontWeight: 400,
+                color: "var(--black)",
+                opacity: 0.6,
+                marginTop: "4px",
+              }}
+            >
+              History delle operazioni svolte
+            </span>
+          </div>
         </div>
       </div>
     </div>
